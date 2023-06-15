aa5b872e7a5ab0dac545f66792eb49fcec43a322
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-06-15 07:18:58 +0000

    [Add]投稿詳細画面実装　削除機能（現時点で不具合あり）

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -4,12 +4,19 @@ class Admin::PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @user = @post.user
+    @comment = @post.comments
   end
 
   def destroy
-    post = Post.find(params[:id])
+    post = Post.find(params[:post_id])
     post.destroy
     redirect_to admin_root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:product_name, :tast, :price, :net, :serving_size, :calorie, :protein, :description, :image)
   end
 
 end
