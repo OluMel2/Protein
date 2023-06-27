c1d8e4c12fd2558fc906bc628075bac1ed5dad6e
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-06-27 13:15:48 +0000

    [Update]退会ユーザーの商品誤表示修正

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -21,9 +21,8 @@ class Public::PostsController < ApplicationController
 
   def index
     @q = Post.ransack(params[:q])
-    @posts = Post.joins(:user).where("users.is_deleted = false")
     #退会したユーザーのデータは表示されない。
-    @posts = @q.result(distinct: true).page(params[:page]).order(created_at: :desc)
+    @posts = @q.result(distinct: true).joins(:user).where("users.is_deleted = false").order(created_at: :desc).page(params[:page])
     if params[:tag_id].present?
       @posts = @posts.where(tag_id: params[:tag_id])
     end
